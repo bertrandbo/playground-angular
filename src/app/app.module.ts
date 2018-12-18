ae8ad8cf82014564bf23ef44a775cd6f2dcc5073
Bertrand <bertrand.bousquet@jooxter.com>
2018-12-18 10:23:39 +0100

    add binding in QuizQuestion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,17 @@ import { NavbarComponent } from './common/navbar/navbar.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { HomeComponent } from './common/home/home.component';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
+import { QuizQuestionComponent } from './quiz-question/quiz-question.component';
 
 @NgModule({
-  declarations: [ AppComponent, NavbarComponent, FooterComponent, HomeComponent, QuizListComponent ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    QuizListComponent,
+    QuizQuestionComponent
+  ],
   imports: [ BrowserModule ],
   bootstrap: [AppComponent]
 })
