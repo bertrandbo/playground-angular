43770b687392a1debfe934e7a0713b0ea1e7f409
Bertrand Bousquet <bertrand.bousquet@jooxter.com>
2018-12-19 17:21:10 +0100

    new quiz form is now plugged
    Still, there is no validation at all...

--- src/app/admin/quiz-form/quiz-form.component.ts
+++ src/app/admin/quiz-form/quiz-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { QuizService } from 'src/app/quiz/services/quiz.service';
+import { Quiz } from 'src/app/models/quiz';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-form',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizFormComponent implements OnInit {
 
-  constructor() { }
+  quizForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private quizService: QuizService, private router: Router) { }
 
   ngOnInit() {
-
+    this.quizForm = this.fb.group({
+      title: [ ],
+      description: [ ],
+      canRetryQuestion: [ false ]
+    });
   }
 
+  saveQuiz() {
+    console.log('saveQuiz', this.quizForm.value);
+    this.quizService.saveQuiz(new Quiz(this.quizForm.value)).subscribe((ret: any) => {
+      this.router.navigate(['/admin/quiz']);
+    });
+  }
 }
