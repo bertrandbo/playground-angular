f788c60acd76d8cb0ab2f1c56ea2ad5662c98085
Bertrand Bousquet <bertrand.bousquet@jooxter.com>
2018-12-19 16:19:12 +0100

    fix observable's operator imports

--- src/app/quiz/services/quiz.service.ts
+++ src/app/quiz/services/quiz.service.ts
@@ -2,6 +2,11 @@ import { Injectable, Inject } from '@angular/core';
 import { Quiz } from '../../models/quiz';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/observable/of';
 import { QuizSubmission } from 'src/app/models/quiz-submission';
 
 @Injectable({
